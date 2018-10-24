b71f5e7b0ec8c05f1c4570e06dc13ff7011d3cbb
athenaozanich <athenaozanich@gmail.com>
2018-10-24 15:16:27 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 2 - List Filter and Pagination
+******************************************/
+
+// Add variables that store DOM elements you will need to reference and/or manipulate
+
+
+
+// Create a function to hide all of the items in the list excpet for the ten you want to show
+// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+
+
+
+
+// Create and append the pagination links - Creating a function that can do this is a good approach
+
+
+
+
+// Add functionality to the pagination buttons so that they show and hide the correct items
+// Tip: If you created a function above to show/hide list items, it could be helpful here
+
+
+
+
+
+
