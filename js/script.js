ac00e64efa8ad4c09e9341523990b8fde6035868
athenaozanich <athenaozanich@gmail.com>
2018-11-01 16:10:00 -0400

    Fixes to pagination

--- js/script.js
+++ js/script.js
@@ -2,65 +2,70 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
+  // Add variables that store DOM elements you will need to reference and/or manipulate
+  let studentList = document.querySelectorAll('.student-item');
+  let page = 0;
+  let listContainer = document.querySelector('.page');
+  let pagination =  document.createElement("UL");
 
-// Add variables that store DOM elements you will need to reference and/or manipulate
-let studentList = document.querySelectorAll('.student-item');
-let page = 0;
-let listContainer = document.querySelector('.page');
-let pagination =  document.createElement("UL");
-let pgContainer = document.querySelectorAll('.pagination li');
+  // Create a function to hide all of the items in the list excpet for the ten you want to show
+  // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
 
-// Create a function to hide all of the items in the list excpet for the ten you want to show
-// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
-
-function showStudents(pgNum){
-
-  let studentsPerPg = pgNum*10;
-  for (var i = 0; i < studentList.length; i++) {
-    studentList[i].style.display = 'none';
-
-    if (i <= studentsPerPg) {
-      console.log(studentsPerPg);
+  function showStudents(pgNum){
+    if (!pgNum) {
+      pgNum = 1;
     }
-  }
-}
+    let studentsPerPg = pgNum*10;
 
-// Create and append the pagination links - Creating a function that can do this is a good approach
-function paginationLinks(){
-  let NumOfStudents = 0;
-
-  listContainer.append(pagination);
-  pagination.classList.add("pagination");
-
-
-  if (studentList.length <= 0) {
-    console.log("Sorry there are no results");
-  }
-
-  for (var i = 0; i < studentList.length; i++) {
-    if (i >= NumOfStudents) {
-      NumOfStudents = NumOfStudents + 10;
-      page++;
-      let pgItem =  document.createElement("LI");
-      pagination.append(pgItem);
-      let pgLink =  document.createElement("A");
-      pgItem.append(pgLink);
-      let pageNum = document.createTextNode(page);
-      pgLink.append(pageNum);
-
+    for (var i = 0; i < studentList.length; i++) {
+      if (i <= studentsPerPg-10) {
+          studentList[i].style.display = 'none';
+      }else if (i <= studentsPerPg-1) {
+        studentList[i].style.display = 'block';
+      }else if (i >= studentsPerPg-1) {
+        studentList[i].style.display = 'none';
+        console.log(studentsPerPg);
+      }
     }
+    console.log(studentsPerPg);
   }
 
-}
-showStudents();
-paginationLinks();
+  // Create and append the pagination links - Creating a function that can do this is a good approach
+  function paginationLinks(){
+    let NumOfStudents = 0;
+
+    listContainer.append(pagination);
+    pagination.classList.add("pagination");
 
 
-// Add functionality to the pagination buttons so that they show and hide the correct items
-// Tip: If you created a function above to show/hide list items, it could be helpful here
-for (var i = 0; i < pgContainer.length; i++) {
-  pgContainer[i].addEventListener("click", function (event) {
-    showStudents(event.target.innerHTML);
-    console.log(event.target.innerHTML);
-  });
-}
+    if (studentList.length <= 0) {
+      console.log("Sorry there are no results");
+    }
+
+    for (var i = 0; i < studentList.length; i++) {
+      if (i >= NumOfStudents) {
+        NumOfStudents = NumOfStudents + 10;
+        page++;
+        let pgItem =  document.createElement("LI");
+        pagination.append(pgItem);
+        let pgLink =  document.createElement("A");
+        pgItem.append(pgLink);
+        let pageNum = document.createTextNode(page);
+        pgLink.append(pageNum);
+
+      }
+    }
+    let pgContainer = document.querySelectorAll('.pagination a');
+      // Add functionality to the pagination buttons so that they show and hide the correct items
+      // Tip: If you created a function above to show/hide list items, it could be helpful here
+      for (var i = 0; i < pgContainer.length; i++) {
+        pgContainer[i].classList.remove('active');
+        pgContainer[i].addEventListener("click", function (event) {
+          showStudents(event.target.innerHTML);
+          event.target.classList.add('active');
+          console.log(event.target);
+        });
+      }
+  }
+  paginationLinks();
+  showStudents();
