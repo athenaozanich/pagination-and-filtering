41fac83cb114cf4b58ed2c4c3574f3d248999092
athenaozanich <athenaozanich@gmail.com>
2018-11-02 00:01:19 -0400

    Fixes to pagination

--- js/script.js
+++ js/script.js
@@ -7,27 +7,30 @@ FSJS project 2 - List Filter and Pagination
   let page = 0;
   let listContainer = document.querySelector('.page');
   let pagination =  document.createElement("UL");
-
+  let pgContainer;
+  let prevPage;
+  paginationLinks();
   // Create a function to hide all of the items in the list excpet for the ten you want to show
   // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
 
   function showStudents(pgNum){
-    if (!pgNum) {
+
+
+    if (!pgNum ) {
       pgNum = 1;
     }
     let studentsPerPg = pgNum*10;
 
     for (var i = 0; i < studentList.length; i++) {
-      if (i <= studentsPerPg-10) {
+      if (i <= studentsPerPg-11) {
           studentList[i].style.display = 'none';
       }else if (i <= studentsPerPg-1) {
         studentList[i].style.display = 'block';
+        pgContainer[pgNum-1].classList.add('active');
       }else if (i >= studentsPerPg-1) {
         studentList[i].style.display = 'none';
-        console.log(studentsPerPg);
       }
     }
-    console.log(studentsPerPg);
   }
 
   // Create and append the pagination links - Creating a function that can do this is a good approach
@@ -55,17 +58,23 @@ FSJS project 2 - List Filter and Pagination
 
       }
     }
-    let pgContainer = document.querySelectorAll('.pagination a');
+    pgContainer = document.querySelectorAll('.pagination a');
       // Add functionality to the pagination buttons so that they show and hide the correct items
       // Tip: If you created a function above to show/hide list items, it could be helpful here
+
       for (var i = 0; i < pgContainer.length; i++) {
-        pgContainer[i].classList.remove('active');
+        let curPage;
+        let prevPage;
+        console.log(prevPage);
         pgContainer[i].addEventListener("click", function (event) {
-          showStudents(event.target.innerHTML);
-          event.target.classList.add('active');
-          console.log(event.target);
+          if (prevPage === curPage) {
+
+          }
+          curPage = event.target;
+          curPage.classList.add('active');
+          showStudents(curPage.innerHTML);
         });
+        prevPage = curPage;
       }
+      showStudents();
   }
-  paginationLinks();
-  showStudents();
